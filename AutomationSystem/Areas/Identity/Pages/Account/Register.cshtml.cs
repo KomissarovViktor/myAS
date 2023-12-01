6cfdbc55e7fee1527938447e491386d6390a8fb6
Viktor Komissarov <vitya.komissarov11@gmail.com>
2023-12-01 03:22:40 +0200

    modified 3

--- AutomationSystem/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ AutomationSystem/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -133,7 +133,7 @@ namespace AutomationSystem.Areas.Identity.Pages.Account
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
-                await _userManager.AddToRoleAsync(user, "Manager");
+                await _userManager.AddToRoleAsync(user, "User");
 
                 if (result.Succeeded)
                 {
